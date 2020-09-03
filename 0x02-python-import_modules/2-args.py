0d5945d608f727c7881bc189a123cf119953c657
Jocelyn Vega <jossvega103@gmail.com>
2020-09-03 21:03:05 +0000

    Modifiy conditionals

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
 # Author: Joss
+
 if __name__ == "__main__":
     import sys
 
     nargs = len(sys.argv) - 1
 
-    if (nargs == 1):
+    if (nargs == 0):
+        print("{:d} arguments.".format(nargs))
+    elif (nargs == 1):
         print("{:d} argument:".format(nargs))
     else:
         print("{:d} arguments:".format(nargs))
 
-    for str in range(len(sys.argv)):
-        if (str == 0):
+    for index in range(len(sys.argv)):
+        if (index == 0):
             continue
-        print("{:d}: {:s}".format(str, sys.argv[str]))
+        print("{:d}: {:s}".format(index, sys.argv[index]))
