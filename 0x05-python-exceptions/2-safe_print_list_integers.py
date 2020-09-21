3039a052c1850f5a53da0ee541d2708acfddb24e
Jocelyn Vega <jossvega103@gmail.com>
2020-09-21 22:57:48 +0000

    Add spicified "ValueError, TypeError"

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -5,6 +5,10 @@ def safe_print_list_integers(my_list=[], x=0):
     count = 0
     if (x > 0):
         for i in range(x):
-            print("{:d}".format(my_list[i]), end="")
-            count += 1
-    return count
+            try:
+                print("{:d}".format(my_list[i]), end="")
+                count += 1
+            except (ValueError, TypeError):
+                continue
+    print()
+    return (count)
