76736fe028133b7ddfb305c080ac4a45b7365398
Jocelyn Vega <jossvega103@gmail.com>
2020-09-28 23:12:32 +0000

    Task 1.1  and task 2.1 modify style pep8

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,41 +1,39 @@
-#!/usr/bin/pyhton3
+#!/usr/bin/python3
 """
-class Rectangle: Define set and get of width and height.
+Class Rectangle: Defines a Rectangle
 """
 
 
 class Rectangle:
-    """ class Square that defines a square """
-
+    """ class that defines a Rectangle with attributes """
     def __init__(self, width=0, height=0):
-        """ Initialize attributes"""
         self.width = width
         self.height = height
 
     @property
     def width(self):
         """ Retrieve the width of rectangle """
-        return self.__width
+        return (self.__width)
 
     @width.setter
     def width(self, value):
-        """set passsed private attribue of width"""
-        if not(isinstance(value, int)):
+        """ set passed private attribue of width """
+        if not (isinstance(value, int)):
             raise TypeError("width must be an integer")
         elif (value < 0):
-            raise TypeError("width must be >= 0")
+            raise ValueError("width must be >= 0")
         self.__width = value
 
     @property
     def height(self):
-        """ Retrieve the width of rectangle """
-        return self.__height
+        """ Retrieve the height of rectangle """
+        return (self.__height)
 
     @height.setter
     def height(self, value):
-        """set passsed private attribue of width"""
-        if not(isinstance(value, int)):
+        """ set passed private attribute of height """
+        if not (isinstance(value, int)):
             raise TypeError("height must be an integer")
         elif (value < 0):
-            raise TypeError("height must be >= 0")
+            raise ValueError("height must be >= 0")
         self.__height = value
