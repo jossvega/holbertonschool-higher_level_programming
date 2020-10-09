efe08bf69d6efe3bcf29756cc114d15b110508ca
Jocelyn Vega <jossvega103@gmail.com>
2020-10-09 04:19:58 +0000

    Advance plus - Write a function that inserts a line of text to a file, after each line containing a specific string (see example):

--- 0x0B-python-input_output/main.py
+++ 0x0B-python-input_output/main.py
@@ -1,21 +1,24 @@
 #!/usr/bin/python3
-"""
+append_after = __import__('100-append_after').append_after
+
+append_after("append_after_100.txt", "Python", "\"C is fun!\"\n")
+
+"""task 14
+
 14-main
-"""
+
 pascal_triangle = __import__('14-pascal_triangle').pascal_triangle
 
 
 def print_triangle(triangle):
-    """
     Print the triangle
-    """
     for row in triangle:
         print("[{}]".format(",".join([str(x) for x in row])))
 
 
 if __name__ == "__main__":
     print_triangle(pascal_triangle(5))
-
+"""
 
 """Task 13
 import os
