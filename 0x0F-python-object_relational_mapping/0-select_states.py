8982e8462b0dfffb61867e2ed8e1891b256a1f9e
jossvega <jossvega103@gmail.com>
2020-12-12 16:36:41 -0500

    Task 0 - Write a script that lists all states from the database hbtn_0e_0_usa

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# List state database hbtn_0e_0_usa
+# Sintax: ./0-select_states.py username password database_name
+from sys import argv
+import MySQLdb
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(host="localhost", user=argv[1], passwd=argv[2],
+                           db=argv[3], port=3306)
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    query_rows = cur.fetchall()
+
+    for row in query_rows:
+        print(row)
+
+    cur.close()
+    conn.close()
