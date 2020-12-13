ef0d9fb1f48841695f49d9a3846fd4c7c5adfdee
jossvega <jossvega103@gmail.com>
2020-12-13 15:09:42 -0500

    Task 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -10,5 +10,13 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(host="192.168.1.55", user=argv[1], passwd=argv[2],
                            conn=argv[3])
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id")
-    [print(state) for state in cur.fetchall() if state[1][0] == "N"]
+    cur.execute("SELECT * FROM states WHERE name\
+                 LIKE BINARY 'N%' ORDER BY states.id ASC")
+    query_rows = cur.fetchall()
+
+    for row in query_rows:
+        print(row)
+    """Close all cursors"""
+    cur.close()
+    """Close all cursors"""
+    conn.close()
