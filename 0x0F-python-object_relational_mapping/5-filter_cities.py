b5bb2e9a68a92f46c5b522337a6c84def41858c2
jossvega <jossvega103@gmail.com>
2020-12-13 19:11:38 -0500

    Quit close and add port

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -5,26 +5,21 @@ of that state, using the database hbtn_0e_4_usa
 """
 
 if __name__ == "__main__":
-
     import MySQLdb
-    from sys import argv
+    import sys
 
-    conn = MySQLdb.connect(host="192.168.1.55", user=argv[1], passwd=argv[2],
-                           db=argv[3])
-    cur = conn.cursor()
-    """, to the end of argv[4] because second paramether is a tuple"""
+    db = MySQLdb.connect(host='localhost', port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+
+    cur = db.cursor()
     cur.execute("""SELECT cities.name
                 FROM cities INNER JOIN states
                 ON states.id = cities.state_id
                 WHERE states.name= %s
                 ORDER BY cities.id ASC""",
-                (argv[4],))
+                (sys.argv[4],))
     query_rows = cur.fetchall()
     array_1 = []
     for row in query_rows:
         array_1.append(row[0])
     print(', '.join(array_1))
-    """Close all cursors"""
-    cur.close()
-    """Close all databases"""
-    conn.close()
