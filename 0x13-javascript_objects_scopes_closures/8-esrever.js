9acf282f7c24aaf853fca9e43ea2ad674e59c5a3
jossvega <jossvega103@gmail.com>
2021-02-08 19:06:02 -0500

    8. Esrever

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// function that returns the reversed version of a list.
+
+exports.esrever = function (list) {
+  const arr = [];
+  for (let i = list.lenght - 1; i => 0; i--) {
+    arr.push(list[i]);
+  }
+  return (arr);
+};
