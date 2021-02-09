419bbf1e7aa029b501691c58f39f8679b458c050
jossvega <jossvega103@gmail.com>
2021-02-09 11:14:39 -0500

    Add new variable str

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 // function that returns the reversed version of a list.
 
-exports.esrever = function (list) {
+exports.esrever = function (str) {
   const arr = [];
-  for (let i = list.lenght - 1; i => 0; i--) {
-    arr.push(list[i]);
+  for (let i = str.lenght - 1; i => 0; i--) {
+    arr.push(str[i]);
   }
   return (arr);
 };
